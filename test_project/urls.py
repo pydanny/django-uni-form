170a96c1c06f5e98a24455b8b5978693e00cef14
venkasub <venkat83@gmail.com>
2011-06-27 19:41:42 +0530

    got the test_project working

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -18,3 +19,9 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # (r'^admin/(.*)', admin.site.root),
 )
+
+
+if settings.DEBUG:
+  urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_DOC_ROOT}),
+  )
